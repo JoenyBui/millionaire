a3676bdc12ffa5d9039595f6187b66b110d24bcb
buij <buij@dnb.com>
2017-11-17 07:47:25 -0600

    Added import.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 
+import * as _ from  'underscore';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
