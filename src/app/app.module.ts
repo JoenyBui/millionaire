8d286d0acb93a056b53b93e979389a27056ff9a2
buij <buij@dnb.com>
2017-11-19 00:22:05 -0600

    Added new permissions.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {environment} from '../environments/environment';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -51,6 +52,7 @@ import { RoomComponent } from './room/room.component';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    AngularFireDatabaseModule,
     AngularFireAuthModule,
     MatButtonModule,
     MatCheckboxModule,
--- src/app/moderator/moderator.component.ts
+++ src/app/moderator/moderator.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material';
-import { Moderator} from '../moderator';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material';
+import {Moderator} from '../moderator';
+import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+
+import {AngularFireDatabase, AngularFireObject} from 'angularfire2/database';
+import {Observable} from 'rxjs/Observable';
+
+// import { FirebaseObjectObservable} from 'angularfire2/database-deprecated';
 
 @Component({
   selector: 'app-host',
@@ -10,16 +15,30 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./moderator.component.css'],
 })
 export class ModeratorComponent implements OnInit {
+  roomRef: AngularFireObject<any>;
+  room: Observable<any>;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-  ) {}
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              db: AngularFireDatabase) {
+    this.roomRef = db.object('room');
+    this.room = this.roomRef.valueChanges();
+  }
 
   ngOnInit() {
   }
 
+  save(room: string, name: string) {
+    return this.roomRef.set({
+      roomTitle: room,
+      name: name
+    });
+  }
+
   createNewRoom() {
-    this.router.navigate(['/moderator-room/1'])
+    const promise = this.save("hi", "bye")
+    promise.then(_ =>
+      this.router.navigate(['/moderator-room/1'])
+    ).catch(err => console.log(err, 'You do not have access'));
   }
 }
