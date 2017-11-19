1a0d46f769e098224eefe0acdc2eb187070277a7
buij <buij@dnb.com>
2017-11-19 15:40:47 -0600

    Update some binding.

--- src/app/moderator/moderator.component.ts
+++ src/app/moderator/moderator.component.ts
@@ -18,6 +18,8 @@ export class ModeratorComponent implements OnInit {
   // roomRef: AngularFireDatabase<any>;
   // room: Observable<any []>;
   db: AngularFireDatabase;
+  name: string;
+  roomName: string;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
@@ -31,17 +33,40 @@ export class ModeratorComponent implements OnInit {
   ngOnInit() {
   }
 
-  save(room: string, name: string) {
-    const roomsRef = this.db.list('rooms');
+  saveNewModerator(name: string) {
+    const moderatorRef = this.db.list('moderators');
 
-    return roomsRef.push({
-      roomTitle: room,
+    return moderatorRef.push({
       name: name
     });
   }
 
+  saveNewRoom(name, mid) {
+    const roomsRef = this.db.list('rooms');
+
+    return roomsRef.push({
+      name: name,
+      mid: mid,
+    });
+  }
+
+  save(roomName: string, name: string) {
+    return new Promise<any>(
+      (resolve, reject) => {
+        this.saveNewModerator(name).then(
+          (val1: any) => this.saveNewRoom(roomName, val1.key).then(
+            (val2) => resolve(val2),
+            (err2) => reject(err2)
+          ),
+          (err1) => reject(err1)
+        );
+      }
+    );
+  }
+
   createNewRoom() {
-    const promise = this.save("hi", "bye");
+    const promise = this.save(this.roomName, this.name);
+
     promise.then(
       (val) =>
         this.router.navigate([`/moderator-room/${val.key}`]),
