02151db2e9d66d6fa03d0e06e77741c737b48144
buij <buij@dnb.com>
2017-11-19 22:57:21 -0600

    Added player passcode and room number connection.

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+
+import {AngularFireDatabase} from 'angularfire2/database';
+import 'rxjs/add/operator/take';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-player',
@@ -8,17 +12,61 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
+  db: AngularFireDatabase;
+  name: string;
+  passcode: string;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-  ) {}
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              db: AngularFireDatabase) {
+    this.db = db;
+  }
 
 
   ngOnInit() {
   }
 
+  saveNewPlayer(name: string, roomid: string) {
+    const playerRef = this.db.list('players');
+
+    return playerRef.push({
+      name: name,
+      roomid: roomid
+    })
+  }
+
+  findGameRoom() {
+    const passcode = this.passcode;
+
+    return new Promise<any>(
+      (resolve, reject) => {
+
+        this.db.object(`codeToMap`).snapshotChanges().subscribe(action => {
+          // TODO: This is bad code - we should just be querying the database and see if this passcode exists.
+          const codeToMap = action.payload.val();
+
+          if (passcode in codeToMap) {
+            resolve({roomId: codeToMap[passcode]});
+          } else {
+            reject('Passcode does not exists');
+          }
+        });
+      }
+    );
+  }
+
   joinGameRoom() {
-    this.router.navigate(['/player-room/1'])
+    const playerName = this.name;
+    this.findGameRoom().then((val) => {
+        const roomid = val.roomId;
+        this.saveNewPlayer(playerName, roomid).then((val1) => {
+          this.router.navigate([`/player-room/${val1.key}`]);
+        }, (err1) => {
+          console.log(err1);
+        };
+      },
+      (err) => {
+        console.log(err);
+      });
   }
 }
