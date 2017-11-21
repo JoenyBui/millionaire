8c3ba9c4376016f50440c04fd14a3f2a7912ee42
buij <buij@dnb.com>
2017-11-20 22:20:28 -0600

    Add presence system.

--- src/app/moderator-detail/moderator-detail.component.ts
+++ src/app/moderator-detail/moderator-detail.component.ts
@@ -3,24 +3,43 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {Location} from '@angular/common';
 
+import {AngularFireDatabase} from 'angularfire2/database';
+
 @Component({
   selector: 'app-moderator-detail',
   templateUrl: './moderator-detail.component.html',
   styleUrls: ['./moderator-detail.component.css']
 })
 export class ModeratorDetailComponent implements OnInit {
-  name: number = null;
+  id: string;
+  name: string;
+  db: AngularFireDatabase;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private location: Location
-  ) {}
-
+    private location: Location,
+    db: AngularFireDatabase
+  ) {
+    this.db = db;
+  }
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    // this.route.paramMap
-    this.name = id;
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.id = id;
+
+    this.db.object(`moderators/${id}`).snapshotChanges().subscribe(action => {
+      const val = action.payload.val();
+      const amOnline = this.db.database.ref('/.info/connected');
+      const userRef = this.db.database.ref(`/moderators/${id}/presence`);
+
+      amOnline.on('value', function (snapshot) {
+        if (snapshot.val()) {
+          userRef.onDisconnect().remove();
+          userRef.set(true);
+        }
+      });
+    });
   }
 }
--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -3,24 +3,42 @@ import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import {AngularFireDatabase} from 'angularfire2/database';
+import 'rxjs/add/operator/take';
+import {Observable} from 'rxjs/Observable';
+
 @Component({
   selector: 'app-player-detail',
   templateUrl: './player-detail.component.html',
   styleUrls: ['./player-detail.component.css']
 })
 export class PlayerDetailComponent implements OnInit {
-  id: number = null;
-
+  id: string;
+  db: AngularFireDatabase;
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private location: Location
-  ) {}
-
-
-  ngOnInit() {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.id = id;
+    private location: Location,
+    db: AngularFireDatabase
+  ) {
+    this.db = db;
   }
 
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.id = id;
+
+    this.db.object(`players/${id}`).snapshotChanges().subscribe(action => {
+      const val = action.payload.val();
+      const amOnline = this.db.database.ref('/.info/connected');
+      const userRef = this.db.database.ref(`/roomPresence/${val.roomid}/${id}`);
+
+      amOnline.on('value', function (snapshot) {
+        if (snapshot.val()) {
+          userRef.onDisconnect().remove();
+          userRef.set(true);
+        }
+      });
+    });
+  }
 }
